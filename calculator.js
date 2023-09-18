1a4376fffa247b60cd4312cc1909f3d6304a2038
Infinitea-3 <rummel.nathan@yahoo.com>
2023-09-17 20:25:45 -0400

    Fixed Number With Multiple Digits Issue, Added Delete Button, Fixed Operator Sign Bug

--- calculator.js
+++ calculator.js
@@ -25,26 +25,48 @@ let operate = function(a,b,operator){
     }
 };
 
-//Grab Numbers/Operators When Clicked
+//Grab Number When Clicked, Concats Onto Existing Number
 const display = document.querySelector('.display');
 const numbers = document.querySelectorAll('.number');
 
-let displayValue = [0];
+let displayValue = [""];
+
 numbers.forEach(button=>{
     button.addEventListener('click', function(){
-        displayValue.push(Number(button.textContent));
+        if(displayValue.length == 0){
+            number = button.textContent;
+            displayValue.push(number);
+        }
+        else{
+        number = displayValue.at(-1) + button.textContent;
+        displayValue = displayValue.slice(0,-1);
+        displayValue.push(number);
+        }
         display.textContent = displayValue.at(-1);
     });
 });
 
+//Grab Operator When Clicked
 const operators = document.querySelectorAll('.operator');
 let operatorValue = [];
 
 operators.forEach(button=>{
     button.addEventListener('click', function(){
-        display.textContent = button.textContent;
-        operatorValue.push(button.textContent);
+        if(displayValue.at(-2)=="+" || displayValue.at(-2)=="-" || displayValue.at(-2)=="*" || displayValue.at(-2)=="/"){
+            displayValue = displayValue.slice(0,-2);
+        }
+        displayValue.push((button.textContent));
+        display.textContent = displayValue.at(-1);
+        displayValue.push("");
     });
+});
+
+//Run 'Operate' When Equal Sign Is Pressed (No Exception Handling Currently)
+const equalSign = document.querySelector('.equalSign');
+equalSign.addEventListener('click',function(){
+    console.log(displayValue);
+    displayValue.push(operate(Number(displayValue.at(-3)), Number(displayValue.at(-1)), displayValue.at(-2)));
+    display.textContent = displayValue.at(-1);
 });
 
 //Flip Sign When +/- Button Pressed
@@ -55,17 +77,22 @@ plusMinus.addEventListener('click',function(){
     display.textContent = displayValue.at(-1);
 });
 
+//Delete Last 
+const deleteButton = document.querySelector('.delete');
+    deleteButton.addEventListener('click',function(){
+        if(Number(displayValue.at(-1)) % 1 == 0){
+            string = displayValue.at(-1);
+            newString = string.substring(0,string.length-1);
+            displayValue = displayValue.slice(0,-1);
+            displayValue.push(newString);
+        }
+    display.textContent = displayValue.at(-1);
+    });
+
 //Clear Arrays When AC Is Pressed
 const resetButton = document.querySelector('.reset');
 resetButton.addEventListener('click',function(){
-    displayValue = [0];
-    operatorValue = [];
+    displayValue = [""];
     display.textContent = 0
 });
 
-//Run 'Operate' When Equal Sign Is Pressed (No Exception Handling Currently)
-const equalSign = document.querySelector('.equalSign');
-equalSign.addEventListener('click',function(){
-    displayValue.push(operate(displayValue.at(-2), displayValue.at(-1), operatorValue.at(-1)));
-    display.textContent = displayValue.at(-1);
-});
